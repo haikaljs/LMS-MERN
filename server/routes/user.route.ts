b5f295e506777eff7caf58279a25f7a58b320b36
haikaljs <haikaljavascript@gmail.com>
2023-10-30 17:59:37 +0800

    Social auth

--- server/routes/user.route.ts
+++ server/routes/user.route.ts
@@ -6,6 +6,7 @@ import {
   loginUser,
   logoutUser,
   registrationUser,
+  socialAuth,
   updateAccessToken,
 } from "../controllers/user.controller";
 import { isAuthenticated } from "../middleware/auth";
@@ -17,5 +18,6 @@ userRouter.post("/login", loginUser);
 userRouter.get("/logout", isAuthenticated, logoutUser);
 userRouter.get("/refresh", updateAccessToken);
 userRouter.get("/me", isAuthenticated, getUserInfo);
+userRouter.post("/social-auth", socialAuth);
 
 export default userRouter;
